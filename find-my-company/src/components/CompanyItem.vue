6a483f93c4b201b1b2a541027b31088f7b43328b
rassatl <lou.rassat2003@gmail.com>
2025-04-03 18:53:55 +0200

    push main

---
+++ find-my-company/src/components/CompanyItem.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="card">
+    <h3>{{ msg }}</h3>
+    <p>Nom de l'entreprise</p>
+    <p>Ville : Poitiers</p>
+    <p>Code Postal : Poitiers</p>
+    <p>Cliquer pour plus d'imformations</p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+  background-color: red;
+}
+.card {
+    background-color: green;
+}
+</style>
---
+++ find-my-company/src/components/ListeDeroulante.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+
+const items = ref([
+  { message: 'Foo' },
+  { message: 'Bar' }
+]);
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in items" :key="item.message">
+      {{ item.message }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 15%;
+  height: 100%;
+  background-color: grey;
+  padding: 10px;
+  margin: 0;
+  list-style-type: none;
+}
+
+li {
+  margin: 10px 0;
+}
+</style>
